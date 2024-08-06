c9d58fdf764664d1f45139e4b19ec71d2b245fdc
rupesh0 <rp15498@gmail.com>
2024-08-07 00:04:23 +0530

    Setup for data loading

---
+++ force-app/main/default/lwc/permissions/dataLoader.js
@@ -0,0 +1,3 @@
+export async function loadData(filters) {
+  console.log(filters);
+}
--- force-app/main/default/lwc/permissions/permissions.js
+++ force-app/main/default/lwc/permissions/permissions.js
@@ -1,4 +1,6 @@
 import { api, LightningElement } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { loadData } from "./dataLoader.js";
 
 export default class Permissions extends LightningElement {
   @api
@@ -9,10 +11,24 @@ export default class Permissions extends LightningElement {
     this.state.filters = value;
   }
 
+  async connectedCallback() {
+    try {
+      this.state.data = await loadData();
+    } catch (ex) {
+      const event = new ShowToastEvent({
+        variant: "error",
+        title: "Run Time Error",
+        message: `An error occurred while loading the data.`
+      });
+      this.dispatchEvent(event);
+    }
+  }
+
   state = {
     filters: {
       profileIds: [],
       permissionSetIds: []
-    }
+    },
+    data: {}
   };
 }
