ffd26ef40cdde84e01786f593ae61bf3630b6990
rupesh0 <rp15498@gmail.com>
2024-08-06 23:18:59 +0530

    Created initial filters for current user

--- force-app/main/default/lwc/myPermissions/myPermissions.js
+++ force-app/main/default/lwc/myPermissions/myPermissions.js
@@ -1,3 +1,24 @@
 import { LightningElement } from "lwc";
+import getCurrentUsersDetails from "@salesforce/apex/MyPermissionsController.getCurrentUsersDetails";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-export default class MyPermissions extends LightningElement {}
+export default class MyPermissions extends LightningElement {
+  filters = {};
+
+  async connectedCallback() {
+    try {
+      const result = await getCurrentUsersDetails();
+      this.filters = {
+        profileIds: [result.currentUsersProfileId],
+        PermissionSetIds: result.currentUsersPermissionSetIds
+      };
+    } catch (ex) {
+      const event = new ShowToastEvent({
+        variant: "error",
+        title: "Run Time Error",
+        message: `An error occurred while fetching the current user details.`
+      });
+      this.dispatchEvent(event);
+    }
+  }
+}
