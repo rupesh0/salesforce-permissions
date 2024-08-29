7fe797bdc7cc2674258e15dc04000dc317575d09
Rupesh Prajapat <rp15498@gmail.com>
2024-08-29 19:54:24 +0530

    Added error handling

--- force-app/main/default/lwc/myPermissions/myPermissions.js
+++ force-app/main/default/lwc/myPermissions/myPermissions.js
@@ -1,11 +1,14 @@
 import { LightningElement } from "lwc";
+import { reduceErrors } from "c/ldsUtils";
 import getCurrentUsersDetails from "@salesforce/apex/MyPermissionsController.getCurrentUsersDetails";
-import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 export default class MyPermissions extends LightningElement {
-  filters = {};
+  filters;
+  error;
+  isLoading;
 
   async connectedCallback() {
+    this.isLoading = true;
     try {
       const result = await getCurrentUsersDetails();
       this.filters = {
@@ -13,12 +16,16 @@ export default class MyPermissions extends LightningElement {
         permissionSetIds: result.currentUsersPermissionSetIds
       };
     } catch (ex) {
-      const event = new ShowToastEvent({
-        variant: "error",
-        title: "Run Time Error",
-        message: `An error occurred while fetching the current user details.`
-      });
-      this.dispatchEvent(event);
+      this.error = reduceErrors(ex);
+    } finally {
+      this.isLoading = false;
     }
+  }
+
+  get labels() {
+    return {
+      error: "An error occurred while fetching the current user details.",
+      loading: "Loading"
+    };
   }
 }
