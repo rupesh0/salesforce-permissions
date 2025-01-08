174c9f3c23e662c8005c51b0744fd5ab3a9c6ab1
Rupesh Prajapat <rp15498@gmail.com>
2025-01-08 10:53:25 +0530

    Using labels in permission set combobox

---
+++ force-app/main/default/lwc/permissionSetCombobox/i18n.js
@@ -0,0 +1,7 @@
+import common_label_permission_set from "@salesforce/label/c.common_label_permission_set";
+import permissions_label_error_on_fetch_permission_set_combobox_options from "@salesforce/label/c.permissions_label_error_on_fetch_permission_set_combobox_options";
+
+export const LABELS = {
+  common_label_permission_set,
+  permissions_label_error_on_fetch_permission_set_combobox_options
+};
--- force-app/main/default/lwc/permissionSetCombobox/permissionSetCombobox.js
+++ force-app/main/default/lwc/permissionSetCombobox/permissionSetCombobox.js
@@ -2,6 +2,7 @@ import { LightningElement, api, wire } from "lwc";
 import getPermissionSetOptions from "@salesforce/apex/CommonController.getPermissionSetOptions";
 import { reduceErrors } from "c/ldsUtils";
 import Toast from "lightning/toast";
+import { LABELS } from "./i18n";
 
 export default class PermissionSetCombobox extends LightningElement {
   @api selectedValues;
@@ -15,7 +16,8 @@ export default class PermissionSetCombobox extends LightningElement {
       const reduceError = reduceErrors(error);
       Toast.show(
         {
-          label: "Error in loading Permission Set combobox options",
+          label:
+            LABELS.permissions_label_error_on_fetch_permission_set_combobox_options,
           message: reduceError,
           mode: "sticky",
           variant: "error"
@@ -28,6 +30,6 @@ export default class PermissionSetCombobox extends LightningElement {
   }
 
   get labels() {
-    return { permisionSet: "Permission set" };
+    return LABELS;
   }
 }
